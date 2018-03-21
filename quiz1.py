594c73ba5479d2c8c1d8d7f456954e287b88281d
Prateer Kumar Roy <prateergamer@gmail.com>
2018-03-21 08:43:11 +0530

    Python code for calculating number of tiles required for a floor.

---
+++ quiz1.py
@@ -0,0 +1,9 @@
+# Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+
+# (1) How many tiles are needed?
+
+print((9*7)+(5*7))
+
+# (2) You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+
+print((17*6)-((9*7)+(5*7)))
