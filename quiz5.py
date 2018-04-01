cb77933afcac62be885a9d84e34c227e59f306b6
unknown <prateergamer@gmail.com>
2018-04-01 19:47:05 +0530

    Use appropriate quotes to fix the bug in the provided string.

---
+++ quiz5.py
@@ -0,0 +1,7 @@
+# TODO: Fix this string!
+'''
+ford_quote = 'Whether you think you can, or you think you can't--you're right.'
+'''
+
+ford_quote = "Whether you think you can, or you think you can't--you're right"
+print(ford_quote)
