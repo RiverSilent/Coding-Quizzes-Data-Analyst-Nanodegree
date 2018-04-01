31c6c50b2ff85f0796879a7b7f502d191b4f8b98
unknown <prateergamer@gmail.com>
2018-04-01 19:06:29 +0530

    Add SQL queries involving arithmetic operators.

--- notes.sql
+++ notes.sql
@@ -63,3 +63,14 @@ LIMIT 10;
 SELECT name,website,primary_poc
   FROM accounts
 WHERE name='Exxon Mobil';
+
+# Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields. 
+
+SELECT id,account_id,standard_amt_usd/standard_qty AS unit_price
+  FROM orders
+LIMIT 10;
+
+# Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end with _usd. (Try to do this without using the total column). Include the id and account_id fields. 
+
+SELECT id,account_id,(poster_amt_usd/(standard_amt_usd+gloss_amt_usd+poster_amt_usd)*100) AS post_per
+  FROM orders;
