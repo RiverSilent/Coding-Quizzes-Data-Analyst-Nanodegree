6e597f96b0952c5a902d2b0dc0473e6207358e3c
unknown <prateergamer@gmail.com>
2018-04-04 20:06:06 +0530

    Add SQL queries demonstrating the LIKE, IN and NOT clauses.

--- notes.sql
+++ notes.sql
@@ -74,3 +74,64 @@ LIMIT 10;
 
 SELECT id,account_id,(poster_amt_usd/(standard_amt_usd+gloss_amt_usd+poster_amt_usd)*100) AS post_per
   FROM orders;
+
+# Use the accounts table to find all the companies whose names start with 'C'. 
+
+SELECT * 
+  FROM accounts
+WHERE name LIKE 'C%';
+
+# Use the accounts table to find all companies whose names contain the string 'one' somewhere in the name.
+
+SELECT * 
+  FROM accounts
+WHERE name LIKE '%one%';
+
+# Use the accounts table to find all companies whose names end with 's'.
+
+SELECT *
+  FROM accounts
+WHERE name LIKE '%s';
+
+# Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.
+
+SELECT name, primary_poc, sales_rep_id
+  FROM accounts 
+WHERE name IN ('Walmart','Target','Nordstrom');
+
+# Use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords.
+
+SELECT *
+  FROM web_events
+WHERE channel IN ('organic','adwords');
+
+# Use the accounts table to find the account name, primary poc, and sales rep id for all stores except Walmart, Target, and Nordstrom
+
+SELECT name,primary_poc,sales_rep_id 
+  FROM accounts 
+WHERE name NOT IN ('Walmart','Target','Nordstrom');
+
+# Use the web_events table to find all information regarding individuals who were contacted via any method except using organic or adwords methods.
+
+SELECT *
+  FROM web_events
+WHERE channel NOT IN ('organic','adwords');
+
+# Use the accounts table to find all the companies whose names do not start with 'C'.
+
+SELECT name
+  FROM accounts
+WHERE NOT name LIKE ('C%');
+
+
+# Use the accounts table to find all companies whose names do not contain the string 'one' somewhere in the name.
+
+SELECT name 
+  FROM accounts
+WHERE NOT name LIKE ('%one%');
+
+# Use the accounts table to find all companies whose names do not end with 's'.
+
+SELECT name
+  FROM accounts
+WHERE NOT name LIKE ('%s');
