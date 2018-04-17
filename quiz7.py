4fdf829843e0bbb4a6573a4d3c5638bded31d555
unknown <prateergamer@gmail.com>
2018-04-17 17:51:32 +0530

    Code snippet demonstrating list indexing.

---
+++ quiz7.py
@@ -0,0 +1,6 @@
+# Use list indexing to determine how many days are in a particular month based on the integer variable month, and store that value in the integer variable num_days
+
+month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
+month_num = 8
+
+print("Number of days in 8th month is ",month_days[month_num-1])
